bff8d537bc851a5249ae92389562e65894b17057
MayaTalukdar <mt3134@columbia.edu>
2020-03-31 15:32:32 -0400

    Update training_data_generator.R
    Amended code to generate datasets without row names.

--- training_data_generator.R
+++ training_data_generator.R
@@ -10,12 +10,12 @@ trainingDataDream <- fread("Data/DREAM_data_intMEMOIR_train.csv")
 trainTrees <- trainingDataDream$ground
 trainingDataFinalOutput <- as.data.frame(sapply(trainTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
 row.names(trainingDataFinalOutput) <- seq(1, nrow(trainingDataFinalOutput))
-write.table(trainingDataFinalOutput, "trainingDataFinalOutput.txt")
+write.table(trainingDataFinalOutput, "trainingDataFinalOutput.txt", row.names = FALSE)
 
 # generate testing data in desired format
 testingDataDream <- fread("Data/DREAM_data_intMEMOIR_test.csv")
 testTrees <- testingDataDream$ground
 testingDataFinalOutput <- as.data.frame(sapply(testTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
 row.names(testingDataFinalOutput) <- seq(1, nrow(testingDataFinalOutput))
-write.table(testingDataFinalOutput, "testingDataFinalOutput.txt")
+write.table(testingDataFinalOutput, "testingDataFinalOutput.txt", row.names = FALSE)
 
