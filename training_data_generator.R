cefa501d785b7bf46957a3f60f7da0dcf59c81d4
MayaTalukdar <mt3134@columbia.edu>
2020-03-31 15:31:05 -0400

    Update training_data_generator.R
    Created code to generate train and test files in desired format using funtions from training_data_generator_functions.R.

--- training_data_generator.R
+++ training_data_generator.R
@@ -1,18 +1,21 @@
-require(phytools)
-require(phangorn)
-source(file = "training_data_generator_functions.R")
-require(data.table)
+# change to match correct path 
+setwd("Desktop/CBMF4761-final-project/")
 
+# source in necessary functions and packages
+source(file = "training_data_generators_functions.R")
+library(data.table)
 
+# generate training data in desired format
+trainingDataDream <- fread("Data/DREAM_data_intMEMOIR_train.csv")
+trainTrees <- trainingDataDream$ground
+trainingDataFinalOutput <- as.data.frame(sapply(trainTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
+row.names(trainingDataFinalOutput) <- seq(1, nrow(trainingDataFinalOutput))
+write.table(trainingDataFinalOutput, "trainingDataFinalOutput.txt")
 
+# generate testing data in desired format
+testingDataDream <- fread("Data/DREAM_data_intMEMOIR_test.csv")
+testTrees <- testingDataDream$ground
+testingDataFinalOutput <- as.data.frame(sapply(testTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
+row.names(testingDataFinalOutput) <- seq(1, nrow(testingDataFinalOutput))
+write.table(testingDataFinalOutput, "testingDataFinalOutput.txt")
 
-
-
-
-
-
-treeData = "(((1_2102111000:136,(2_2100101000:11,3_2100101000:11):124):45,(((4_2121101121:4,5_2121101121:4):69,(6_2121101101:12,7_2121101101:12):61):57,((8_2211100221:12,13_2111101221:12):63,(15_2110101221:22,16_2111101221:22):53):55):50):31,(((17_2122111100:35,18_2122111100:35):43,(19_2122111100:24,20_2122111100:24):54):53,((21_0111001001:17,22_0111001001:17):59,(23_2101101001:21,24_0101001001:21):55):55):80);"
-tree = read.newick(text = treeData)
-terminal_vector <- sapply(seq(1, length(tree$tip.label)), function(x) generate_terminal_vector_funct(x))
-names(terminal_vector) <- tree$tip.label
-View(as.data.frame(terminal_vector))
