fdbc728286a6b3d76a23d1a22fd02ab3ba43b987
jg4159 <56453071+jg4159@users.noreply.github.com>
2020-04-19 23:58:52 -0400

    Add files via upload

--- Tree_reconstruction_evaluation.R
+++ Tree_reconstruction_evaluation.R
@@ -1,3 +1,6 @@
+library(phangorn)
+library(phytools)
+
 # Use the distance matrix learned from the ground-truth
 score_matrix <- rbind(c(2,6,10),c(6,0,4),c(10,4,3))
 
@@ -26,26 +29,14 @@ Tree.reconstruction <- function(barcodes, score_matrix){
   }
   # generate row and col names for the distance matrix m
   i <- 1:l
-  names <- sapply(i, function(k) paste(k,barcodes[k], sep='-'))
+  names <- sapply(i, function(k) paste(k,barcodes[k], sep='_'))
   row.names(m) <- names
   colnames(m) <- names
   return(m)
 }
 
-# Use the tree with DreamID = 1 to test
-barcode_1 = c('2012210001', '2012210001', '2212210001', '2112210001')
-DM.1 <- Tree.reconstruction(barcode_1, score_matrix)
-DM.1
 
-# Generate a tree by UPGMA
-upgma.tree <- upgma(DM.1, method = "average")
-plot(upgma.tree, main="UPGMA")
-
-# Generate a tree by Neighbor Joining
-nj.tree <- NJ(DM.1)
-plot(nj.tree, main="NJ")
-
-# Test another tree
+# Test on a tree wiht 9 leaves
 barcode_x <- c('2012212021', '2112212021', '2112212021', '2112212021', '0012212221', '0012012221', '2120010021', '2120010021', '0112212221')
 DM <- Tree.reconstruction(barcode_x, score_matrix)
 DM
@@ -55,3 +46,15 @@ plot(upgma.tree, main="UPGMA")
 # Generate a tree by Neighbor Joining
 nj.tree <- NJ(DM)
 plot(nj.tree, main="NJ")
+
+# Evaluate normalized RF
+
+# Load the ground truth tree in Newick format
+tree <- '((((1_2012212021:8,2_2112212021:8):38,(3_2112212021:4,4_2112212021:4):42):42,(5_0012212221:1,6_0012012221:1):87):46,((7_2120010021:22,8_2120010021:22):51,9_0112212221:74):62);'
+ground.truth <- read.newick(text = tree)
+plot(ground.truth, main = "ground_truth")
+#check normalized RF, caution: trees must have identical labels
+RF.upgma <- RF.dist(upgma.tree, ground.truth, normalize = T)
+RF.upgma
+RF.nj <- RF.dist(nj.tree, ground.truth, normalize = T)
+RF.nj
