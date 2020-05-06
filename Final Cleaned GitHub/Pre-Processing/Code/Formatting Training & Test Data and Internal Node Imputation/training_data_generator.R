1c1823d43bf70a9233138dcba16fc490919e293b
MayaTalukdar <mt3134@columbia.edu>
2020-05-06 12:19:14 -0400

    Update training_data_generator.R
    Updated paths based on new directory structure.

--- Final Cleaned GitHub/Pre-Processing/Code/Formatting Training & Test Data and Internal Node Imputation/training_data_generator.R
+++ Final Cleaned GitHub/Pre-Processing/Code/Formatting Training & Test Data and Internal Node Imputation/training_data_generator.R
@@ -1,21 +1,19 @@
-# change to match correct path 
-setwd("Desktop/CBMF4761-final-project/")
 
 # source in necessary functions and packages
 source(file = "training_data_generators_functions.R")
 library(data.table)
 
 # generate training data in desired format
-trainingDataDream <- fread("Data/DREAM_data_intMEMOIR_train.csv")
+trainingDataDream <- fread("../Data/DREAM_data_intMEMOIR_train.csv")
 trainTrees <- trainingDataDream$ground
 trainingDataFinalOutput <- as.data.frame(sapply(trainTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
 row.names(trainingDataFinalOutput) <- seq(1, nrow(trainingDataFinalOutput))
 write.table(trainingDataFinalOutput, "trainingDataFinalOutput.txt", row.names = FALSE)
 
 # generate testing data in desired format
-testingDataDream <- fread("Data/DREAM_data_intMEMOIR_test.csv")
+testingDataDream <- fread("../Data/DREAM_data_intMEMOIR_test.csv")
 testTrees <- testingDataDream$ground
 testingDataFinalOutput <- as.data.frame(sapply(testTrees, function(x) convert_DREAM_tree_to_training_data_row_funct(x)))
 row.names(testingDataFinalOutput) <- seq(1, nrow(testingDataFinalOutput))
-write.table(testingDataFinalOutput, "testingDataFinalOutput.txt", row.names = FALSE)
+write.table(testingDataFinalOutput, "../Data/testingDataFinalOutput.txt", row.names = FALSE)
 
